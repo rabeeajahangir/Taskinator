6dd6a03713a70afefde1a937976dec336491a395
Your Name <you@example.com>
2021-05-03 21:21:18 -0400

    add JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,11 @@
 var buttonEl = document.querySelector("#save-task");
-console.log(buttonEl);
-buttonEl.addEventListener("click", function(){
-    alert("button clicked");
-});
+var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-taskItemEl = document.createElement("li");
-taskItemEl.textContent = "hello";
-taskItemEl;
-
-var buttonEl = document.querySelector("#save-task");
-var tasksToDoEl= document.querySelector("#tasks-to-do");
-
-buttonEl.addEventLsitener("click", function() {
-    var listItemEl = document.createElement("li");
-    listItenEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-
-});
-var createTaskHandler = function () {
-    var listItemEl = document.createElement("li");
-    listItemEl.className = "task-item";
-    listItenEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl)
+var createTaskHandler = function() {
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
+  listItemEl.textContent = "This is a new task.";
+  tasksToDoEl.appendChild(listItemEl);
 };
 
 buttonEl.addEventListener("click", createTaskHandler);
